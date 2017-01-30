4954634c35823e58fbe92117250326386c2488af
ha-li <ha-li@gmail.com>
2017-01-29 22:35:50 -0800

    add gstring constructor and deep copy constructor, along with tests

--- string-utils/src/main/java/com/gecko/lang/Gstring.java
+++ string-utils/src/main/java/com/gecko/lang/Gstring.java
@@ -1,9 +1,38 @@
 package com.gecko.lang;
 
+import java.util.Arrays;
+
 /**
+ * An immutable string class, for the sake of learning immutable and strings.
+ * An immutable object must be final, so that other class does not extend it.
+ *
  * Created by hlieu on 01/28/17.
  */
-public class Gstring {
+public final class Gstring
+   implements java.io.Serializable {
+
+   private static final long serialVersionUID = 8260967008777211795L;
+
+   /** The value used for character storage. */
+   private final char[] value;
+
+   public Gstring () {
+      value = new char[0];
+   }
+
+   public Gstring (char[] val) {
+      value = Arrays.copyOf(val, val.length);
+   }
+
+   /**
+    * This is a deep copy constructor, it copies over the underlying
+    * character sequence.
+    * @param original
+    */
+   public Gstring (Gstring original) {
+      this.value = Arrays.copyOf(original.value, original.value.length);
+   }
+
    public int doNothing () {
      return 0;
    }
--- string-utils/src/test/java/com/gecko/lang/GstringTest.java
+++ string-utils/src/test/java/com/gecko/lang/GstringTest.java
@@ -1,24 +1,69 @@
 package com.gecko.lang;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.lang.reflect.Field;
 
 /**
  * Created by hlieu on 01/29/17.
  */
 public class GstringTest {
+   /**
+    * The resource under test.
+    */
+   private Gstring gstring;
 
-   private Gstring underTest;
-   {
-      underTest = new Gstring ();
+   @Before
+   public void setUp(){
+      gstring = new Gstring ();
    }
 
    @Test
-   public void test_doNothing () {
-      int toVerify = underTest.doNothing();
-      Assert.assertEquals (toVerify, 0);
+   public void test_instantiation () throws NoSuchFieldException, IllegalAccessException {
+      char[] a = {'a', 'b', 'c'};
+
+      Gstring g1 = new Gstring (a);
+      Field f = Gstring.class.getDeclaredField ("value");
+      f.setAccessible (Boolean.TRUE);
+      char[] value = (char[]) f.get(g1);
+
+      Assert.assertNotSame(a, value);
+   }
+
+   @Test
+   public void test_copy_instantiation () throws NoSuchFieldException, IllegalAccessException {
+      char[] a = {'a', 'b', 'c'};
+      Gstring g1 = new Gstring(a);
+      Gstring g2 = new Gstring(g1);
+
+      Field f = Gstring.class.getDeclaredField ("value");
+      f.setAccessible (Boolean.TRUE);
+
+      char[] value1 = (char[]) f.get(g1);
+      char[] value2 = (char[]) f.get(g2);
+
+      Assert.assertNotSame (value1, value2);
+      Assert.assertNotSame (g1, g2);
+   }
+
+   @Test
+   public void test_string_copy () throws NoSuchFieldException, IllegalAccessException{
+      String s1 = new String("abc");
+
+      // this is string's shallow copy, even though it is a different
+      // object, the underlying char[] is the same as s1's char[]
+      String s_copy = new String(s1);
+
+      Field f = String.class.getDeclaredField ("value");
+      f.setAccessible (Boolean.TRUE);
+
+      char[] v1 = (char[]) f.get(s1);
+      char[] v2 = (char[]) f.get(s_copy);
+
+      Assert.assertSame (v1, v2);
+      Assert.assertNotSame (s1, s_copy);
    }
 
 }
